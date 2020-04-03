5068fe6f76af1d81765542540f06f79632605111
DanielNetzer <daniel.netzer@vonage.com>
2020-04-03 11:29:10 +0300

    feat(bot): integrated with Vonage AI bot platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import ChatBot from 'react-simple-chatbot';
 import './App.css';
+
+export function generateSession(date: Date): string {
+  let d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('/');
+}
+
+const newDate = new Date();
+const sessionId = generateSession(newDate);
+const apiKey =
+  'z1tKfnDm0QUKKn+OqhJxLUaPIoQG8DMSsk83Si3WxyXgLNXXXCqpzP2c90S2r+SySL+4YIP16EPnuWq6KYAI53MR6rc+Y8M24KV/OrnCay56xjOjyFeGnws+imK1HSZYRfJlGGnheZ7uXA==';
 
 const steps = [
   {
     id: '0',
-    message: 'Welcome to react chatbot!',
+    message: 'ברוכים הבאים לחמ״ל קורונה מקבוצת מדעת, מה תרצו לדעת?',
     trigger: '1'
   },
   {
     id: '1',
-    message: 'Bye!',
-    end: true
+    user: true,
+    trigger: '2'
+  },
+  {
+    id: '2',
+    message: async ({ value }: { value: string }) => {
+      const body = JSON.stringify({
+        userId: 'midaat',
+        input: value,
+        sessionId
+      });
+
+      const httpResponse = await fetch('https://api.over.ai/api/ai/call', {
+        method: 'POST',
+        body,
+        headers: {
+          'api-key': apiKey,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const nluResponse = await httpResponse.json();
+      console.log(nluResponse);
+      const botResponse = nluResponse.say.sentencesAsOneLine;
+      console.log(botResponse);
+      return botResponse;
+    },
+    trigger: async ({ value }: { value: string }) => {
+      console.log(value);
+      return '0';
+    }
   }
 ];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">בוט חמ״ל קורונה - מידעת</header>
       <section>
         <ChatBot steps={steps} />
       </section>
